9af63bc463984dc85bfa6a1a4f905667bb2016c6
SaikinHub <syllasall@yahoo.fr>
2023-10-21 16:56:58 +0200

    Added webhooks for user management

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -33,21 +33,17 @@ const QuestionCard = ({
   createdAt,
 }: QuestionsProps) => {
   return (
-    <Link
-      href={`/question/${_id}`}
-      className="card-wrapper rounded-[10px] p-9 sm:px-11"
-    >
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-        <div>
-          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {getTimeStamp(createdAt)}
-          </span>
+    <div className="card-wrapper flex flex-col  items-start justify-between gap-5 rounded-[10px] p-9 sm:px-11">
+      <div>
+        <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+          {getTimeStamp(createdAt)}
+        </span>
+        <Link href={`/question/${_id}`}>
           <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
             {title}
           </h3>
-        </div>
+        </Link>
       </div>
-      {/* If signed in add edit delete actions */}
 
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
@@ -87,7 +83,7 @@ const QuestionCard = ({
           textStyles="small-medium text-dark400_light800"
         />
       </div>
-    </Link>
+    </div>
   );
 };
 
