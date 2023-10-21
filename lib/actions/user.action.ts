34a9f02906e59fdc23af17922c4e011a70a7ac94
SaikinHub <syllasall@yahoo.fr>
2023-10-21 17:06:53 +0200

    added eslint ignore line

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -64,6 +64,7 @@ export async function deleteUser(params: DeleteUserParams) {
       throw new Error('User not found');
     }
 
+    // eslint-disable-next-line no-unused-vars
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       '_id'
     );
