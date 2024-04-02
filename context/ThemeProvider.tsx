c284c46db69c3314a57f0dfd20effa7362d925a0
SaikinHub <syllasall@yahoo.fr>
2024-04-02 14:06:39 +0200

    reverted default mode back to "Lightmode"

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,53 +1,55 @@
-"use client"
+'use client';
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 interface ThemeContextType {
-    mode: string;
-    setMode: (mode: string) => void;
+  mode: string;
+  setMode: (mode: string) => void;
 }
 
-const ThemeContext = 
-createContext<ThemeContextType | undefined>(undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({ children}: {children: React.ReactNode}) {
-    const [mode, setMode] = useState('');
-    
-    const handleThemeChange = () => {
-        if(mode === 'dark' || 
-        (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)"
-        ).matches)) {
-            // setMode('light');
-            document.documentElement.classList.add("dark");
-        } else {
-            // setMode('dark');
-            document.documentElement.classList.remove("dark");
-        }
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState('');
+
+  const handleThemeChange = () => {
+    if (
+      mode === 'dark'
+      //   ||
+      //   (!('theme' in localStorage) &&
+      //     window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
     }
+  };
 
-    useEffect(() => {
-        const theme = localStorage.getItem('theme');
-        if(theme) {
-            setMode(theme)
-        }
-    }, [])
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      setMode(theme);
+    }
+  }, []);
 
-    useEffect(() => {
-      handleThemeChange();
-    }, [mode])
-    
-    return (
-        <ThemeContext.Provider value={{mode, setMode}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
 export function useTheme() {
-    const context = useContext(ThemeContext);
-    if(context === undefined) {
-        throw new Error ('useTheme must be used within a ThemeProvider')
-    }
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
 
-    return context;
+  return context;
 }
