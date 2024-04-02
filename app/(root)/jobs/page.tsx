be1746a3f1109f5f4f3e4d1f04845b06762f847d
SaikinHub <syllasall@yahoo.fr>
2024-04-02 13:42:05 +0200

    Added style to placeholder

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -1,5 +1,9 @@
 const page = () => {
-  return <div>This page is still in construction !</div>;
+  return (
+    <div className="body-regular text-dark500_light700 my-3.5 max-w-md">
+      This page is still in construction !
+    </div>
+  );
 };
 
 export default page;
