309226251562c44edd19246f368e08f4ff17e62d
SaikinHub <syllasall@yahoo.fr>
2024-04-04 23:45:11 +0200

    minor UI ajustments

--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 interface MetricProps {
   imgUrl: string;
-  title: string;
+  title?: string;
   alt: string;
   value: string | number;
   textStyles: string;
@@ -26,13 +26,15 @@ const Metric = ({
       <Image
         src={imgUrl}
         alt={alt}
-        width={16}
-        height={16}
+        width={20}
+        height={20}
         className={`object-${
           href ? 'cover rounded-full w-[20px] h-[20px]' : 'contain'
         }`}
       />
-      <p className={`${textStyles} small-medium flex items-center gap-1`}>
+      <p
+        className={`${textStyles} small-medium flex items-center gap-1 mt-[0.1rem]`}
+      >
         {value}{' '}
         <span
           className={`small-regular line-clamp-1 ${
@@ -47,12 +49,14 @@ const Metric = ({
 
   if (href) {
     return (
-      <Link href={href} className="flex-center gap-1">
+      <Link href={href} className="flex-center gap-1.5">
         {metricContent}
       </Link>
     );
   }
-  return <div className="flex-center flex-wrap gap-1">{metricContent}</div>;
+  return (
+    <div className="flex-center flex-wrap gap-[0.2rem]">{metricContent}</div>
+  );
 };
 
 export default Metric;
